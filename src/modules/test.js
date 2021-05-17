f9c1e8aa4301b12294959464cd37cd1718d4a7e7
iv13-web <iivanovspg@gmail.com>
2021-05-17 08:39:06 +0300

    dark theme file created

--- src/modules/test.js
+++ src/modules/test.js
@@ -12,6 +12,34 @@ export class LogSomeData {
     }
 }
 
-document.querySelector('.toggler').addEventListener('click', () => {
-    document.querySelector('.toggler').classList.toggle('toggler_active');
-})
+
+// DARK THEME
+const btn = document.querySelector(".toggler")
+
+btn.addEventListener('click', () => {
+    btn.classList.toggle('toggler_active');
+
+    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)")
+
+    prefersDarkScheme.matches 
+      ? document.body.classList.toggle("light-theme")
+      : document.body.classList.toggle("dark-theme")
+
+});
+
+
+
+
+
+
+
+
+
+
+
+// BURGER
+document.querySelector('.burger').addEventListener('click', () => {
+    document.querySelector('.burger').classList.toggle('burger_active');
+});
+
+
