fca50972ad1074303da8ebe67fe022edb2fcdb31
Igor <iivanovspg@gmail.com>
2021-06-28 09:38:13 +0300

    Grid container for cards

--- src/components/catalog/card.functions.js
+++ src/components/catalog/card.functions.js
@@ -12,13 +12,15 @@ export function renderCard(movie) {
 
 		const title = movie.title ? movie.title : movie.name
 		return `
-			<div class="card" data-id="${movie.id}">
-				<div class="play-btn">
-					<i class="fab fa-youtube" data-type="play"></i>
+			<div class="col_xs-6 col_md-4 col_lg-2">
+				<div class="card" data-id="${movie.id}">
+					<div class="play-btn">
+						<i class="fab fa-youtube" data-type="play"></i>
+					</div>
+					<img class="card__img" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${title}" data-type="card">
+					${rating}
+					<p class="card__text">${title}</p>
 				</div>
-				<img class="card__img" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${title}" data-type="card">
-				${rating}
-				<p class="card__text">${title}</p>
 			</div>
 		`
 	}
